48c1962e9853fac5c160eba9cb974f9c604eef75
ajitkumar-tech <ajitsanjeevi229@gmail.com>
2026-04-12 10:52:25 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "is_enabled" {
+ type = bool
+ default = true
+}
+
+variable "availability_zones" {
+type = list(string)
+default = ["ap-south-1a", "ap-south-1b"] 
+}
+
+variable "ingress-rules" {
+  type = list(number)
+  default = [22,8080,80,443,2049]
+}
+
+variable "egress-rules" { 
+  type = list(number)
+  default = [22,8080,80,443,2049]
+}
