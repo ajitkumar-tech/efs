8d47a6fcf2de1fc59c8fc19a119c2e25d06d8a99
ajitkumar-tech <ajitsanjeevi229@gmail.com>
2026-04-12 16:38:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -21,5 +21,5 @@ variable "egress-rules" {
 variable "subnet_ids" {
   description = "Default subnet IDs for EFS mount targets"
   type        = list(string)
-  default = ["subnet-0844265ec93053b2d", "subnet-0383af57f3f523954"]
+  default = ["subnet-087ed570ca64886dd", "subnet-06f6dc639b36423c4"]
 }
