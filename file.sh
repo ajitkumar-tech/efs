f4a782b9b5533e06b73a425897905630d080e061
ajitkumar-tech <ajitsanjeevi229@gmail.com>
2026-04-12 15:40:20 +0530

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+MOUNT_POINT="/efs"
+
+if [ -d "$MOUNT_POINT" ]; then
+    echo "Directory exists"
+
+else
+                                                                            
+ID=$(cat /etc/os-release | grep -w ID | cut -d "=" -f2 |tr -d '"')
+
+fsid="fs-03f3d8317fd88aed1.efs.ap-south-1.amazonaws.com"
+                                               
+function_ubuntu(){   
+sudo apt update -y && sudo apt install nfs-common -y
+
+}
+
+
+function_redhat(){    
+
+sudo yum update -y && sudo yum install nfs-utils -y
+
+}
+
+if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then          
+
+  function_ubuntu
+
+
+elif [ "$ID" = "rhel" ] || [ "$ID" = "amzn" ]; then   
+                                                                   
+  function_redhat                                                                               
+
+else
+
+echo "notfound"          
+
+
+  fi 
+  fi
+
+
+sudo mkdir $MOUNT_POINT
+
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $fsid:/ $MOUNT_POINT
+
+echo "$fsid:/ $MOUNT_POINT nfs4 defaults,_netdev 0 0" | sudo tee -a /etc/fstab    
